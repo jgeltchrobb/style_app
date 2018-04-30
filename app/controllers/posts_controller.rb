18b61a0caa19cb25edfa2015dc5b8838a0a10bdc
Jordan Geltch-Robb <jgeltchrobb@hotmail.com>
2018-05-01 09:21:48 +1000

    rolify added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,17 +14,21 @@ class PostsController < ApplicationController
 
   # GET /posts/new
   def new
+    not_authorised and return unless current_user.can_create?(@post)
     @post = Post.new
   end
 
   # GET /posts/1/edit
   def edit
+    not_authorised and return unless current_user.can_update?(@post)    
   end
 
   # POST /posts
   # POST /posts.json
   def create
+    not_authorised and return unless current_user.can_create?(@post)
     @post = Post.new(post_params)
+    @post.user = current_user
 
     respond_to do |format|
       if @post.save
@@ -40,6 +44,8 @@ class PostsController < ApplicationController
   # PATCH/PUT /posts/1
   # PATCH/PUT /posts/1.json
   def update
+    not_authorised and return unless current_user.can_update?(@post)
+
     respond_to do |format|
       if @post.update(post_params)
         format.html { redirect_to @post, notice: 'Post was successfully updated.' }
@@ -54,6 +60,8 @@ class PostsController < ApplicationController
   # DELETE /posts/1
   # DELETE /posts/1.json
   def destroy
+    not_authorised and return unless current_user.can_destroy?(@post)
+
     @post.destroy
     respond_to do |format|
       format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
@@ -70,5 +78,10 @@ class PostsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
       params.require(:post).permit(:content, :image, :owner_role)
+    end
+
+    def not_authorised
+      flash[:notice] = "You are not authorised!"
+      redirect_to posts_path
     end
 end
