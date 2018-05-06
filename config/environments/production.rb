093915b01becebc92e6bf7ef72c06bc3a5d65aae
Jordan Geltch-Robb <jgeltchrobb@hotmail.com>
2018-05-06 22:54:52 +1000

    add schema to gitignore

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,14 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  ActionMailer::Base.smtp_settings = {
+    address:              'smtp.sendgrid.net',
+    port:                 '587',
+    domain:               'coderacademy.com.au',
+    user_name:            ENV['SENDGRID_USER'],
+    password:             ENV['SENDGRID_PASS'],
+    authentication:       'plain',
+    # enable_starttls_auto: true 
+  }
 end
