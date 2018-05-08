6b96fcb837f20d6bdf1afe87560255f991e45d36
Jordan Geltch-Robb <jgeltchrobb@hotmail.com>
2018-05-09 00:22:22 +1000

    attempting to handle mailer errors for heroku x2

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,9 +27,9 @@ class User < ApplicationRecord
   def signup_email
     begin
       UserSignupMailer.send_signup_email(self).deliver
-      flash[:success] = "#{@user.name} created"
+      flash[:success] = "Account created"
       rescue Net::SMTPAuthenticationError, Net::SMTPServerBusy, Net::SMTPSyntaxError, Net::SMTPFatalError, Net::SMTPUnknownError => e
-        flash[:success] = "Utente #{@user.name} creato. Problems sending mail"
+        flash[:success] = "Account created but an issue sending email"
       end
       # redirect_to "/"
   end
