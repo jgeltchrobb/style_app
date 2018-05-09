1733a9c22bc3e4ba5d9fd2676616e44d1c5957f3
Jordan Geltch-Robb <jgeltchrobb@hotmail.com>
2018-05-09 16:10:59 +1000

    added serving of static assets to try and get default image working on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,6 +81,8 @@ Rails.application.configure do
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
+  config.serve_static_assets = true
+
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
